59ce74009f7148a901220dd820b91f1a76d566e8
FaridNurArif <210414003@fellow.lpkia.ac.id>
2024-07-14 12:48:01 +0700

    responsive pembayaran

--- resources/views/troli/troli.blade.php
+++ resources/views/troli/troli.blade.php
@@ -28,10 +28,15 @@
         text-align: center;
     }
 
+    .table-wrapper {
+        overflow-x: auto;
+    }
+
     .troli-table {
         width: 100%;
         border-collapse: collapse;
         margin-bottom: 2em;
+        min-width: 600px;
     }
 
     .troli-table th, .troli-table td {
@@ -112,9 +117,18 @@
             max-width: 60px;
         }
 
+        .troli-table input[type="number"] {
+            width: 50px;
+        }
+
         .troli-container button {
             width: 100%;
             margin-bottom: 1em;
+        }
+
+        .action-column {
+            display: flex;
+            justify-content: center;
         }
     }
 </style>
@@ -124,54 +138,56 @@
 
 <div class="troli-container">
     <h1>Shopping Cart</h1>
+    <div class="table-wrapper">
         <table class="troli-table">
-        <thead>
-            <tr>
-                <th>Nama Produk</th>
-                <th>Detail Produk</th>
-                <th>Harga</th>
-                <th>Qty</th>
-                <th class="total-column">Total</th>
-                <th class="action-column">Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php $grandTotal = 0; @endphp
-            @foreach ($carts as $cart)
-                @php
-                    $total = $cart->product->product_price * $cart->qty;
-                    $grandTotal += $total;
-                @endphp
-                <tr data-id="{{ $cart->id }}">
-                    <td>{{ $cart->product->product_name }}</td>
-                    <td>
-                        Brand: {{ $cart->product->brand }}<br>
-                        Tipe: {{ $cart->product->type }}<br>
-                        {{ $cart->product->product_details }}
-                    </td>
-                    <td>{{ $cart->product->product_price }}</td>
-                    <td>
-                        <input type="number" value="{{ $cart->qty }}" class="qty-input" data-price="{{ $cart->product->product_price }}" maxlength="3" min="1" max="999">
-                    </td>
-                    <td class="total-column">
-                        <span class="total">{{ $total }}</span>
-                    </td>
-                    <td class="action-column">
-                        <button class="delete-btn">
-                            <i class="fas fa-trash"></i>
-                        </button>
-                    </td>
+            <thead>
+                <tr>
+                    <th>Nama Produk</th>
+                    <th>Detail Produk</th>
+                    <th>Harga</th>
+                    <th>Qty</th>
+                    <th class="total-column">Total</th>
+                    <th class="action-column">Aksi</th>
                 </tr>
-            @endforeach
-            <tr>
-                <td colspan="4">Total Keseluruhan</td>
-                <td class="total-column">
-                    <span id="grandTotal">{{ $grandTotal }}</span>
-                </td>
-                <td></td>
-            </tr>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @php $grandTotal = 0; @endphp
+                @foreach ($carts as $cart)
+                    @php
+                        $total = $cart->product->product_price * $cart->qty;
+                        $grandTotal += $total;
+                    @endphp
+                    <tr data-id="{{ $cart->id }}">
+                        <td>{{ $cart->product->product_name }}</td>
+                        <td>
+                            Brand: {{ $cart->product->brand }}<br>
+                            Tipe: {{ $cart->product->type }}<br>
+                            {{ $cart->product->product_details }}
+                        </td>
+                        <td>{{ $cart->product->product_price }}</td>
+                        <td>
+                            <input type="number" value="{{ $cart->qty }}" class="qty-input" data-price="{{ $cart->product->product_price }}" maxlength="3" min="1" max="999">
+                        </td>
+                        <td class="total-column">
+                            <span class="total">{{ $total }}</span>
+                        </td>
+                        <td class="action-column">
+                            <button class="delete-btn">
+                                <i class="fas fa-trash"></i>
+                            </button>
+                        </td>
+                    </tr>
+                @endforeach
+                <tr>
+                    <td colspan="4">Total Keseluruhan</td>
+                    <td class="total-column">
+                        <span id="grandTotal">{{ $grandTotal }}</span>
+                    </td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
     <button onclick="window.location.href='{{ url('/produk') }}'">Kembali</button>
     <button onclick="checkout()">Check out</button>
 </div>
